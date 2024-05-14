d277ad5be49d96550d61e136b0386667d3f0c7d2
rsm0128 <rsm0128@hotmail.com>
2024-05-14 12:23:26 -0400

    updated test

--- literati-example/tests/PluginTest.php
+++ literati-example/tests/PluginTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * PlugTest class file.
+ *
+ * @package Literati\Example\Tests
+ */
 
 namespace Literati\Example\Tests;
 
@@ -6,45 +11,81 @@ use Literati\Example\Plugin;
 use WP_Mock\Tools\TestCase as TestCase;
 use WP_Mock;
 
+/**
+ * PluginTest class definition.
+ */
 final class PluginTest extends TestCase {
-  public function setUp(): void {
-    WP_Mock::setUp();
+	public function setUp(): void {
+		WP_Mock::setUp();
 
-    /** Setup mocks */
-    WP_Mock::userFunction('get_option', [
-      'return' => function ($key) {
-        switch ($key) {
-          case 'LITERATI_EXAMPLE_VERSION':
-            return null;
-        }
-      },
-    ]);
+		/** Setup mocks */
+		WP_Mock::userFunction(
+			'get_option',
+			array(
+				'return' => function ( $key ) {
+					switch ( $key ) {
+						case 'LITERATI_EXAMPLE_VERSION':
+							return null;
+					}
+				},
+			)
+		);
 
-    WP_Mock::userFunction('remove_action', [
-      'return' => true,
-    ]);
+		WP_Mock::userFunction(
+			'remove_action',
+			array(
+				'return' => true,
+			)
+		);
 
-    WP_Mock::userFunction('plugin_dir_path', [
-      'return' => function ($dir) {
-        return $dir . '/../';
-      },
-    ]);
+		WP_Mock::userFunction(
+			'has_action',
+			array(
+				'return' => true,
+			)
+		);
 
-    WP_Mock::userFunction('trailingslashit', [
-      'return' => function ($path) {
-        return rtrim($path, '/') . '/';
-      },
-    ]);
-  }
+		WP_Mock::userFunction(
+			'plugin_dir_path',
+			array(
+				'return' => function ( $dir ) {
+					return $dir . '/../';
+				},
+			)
+		);
 
-  public function tearDown(): void {
-    WP_Mock::tearDown();
-  }
+		WP_Mock::userFunction(
+			'trailingslashit',
+			array(
+				'return' => function ( $path ) {
+					return rtrim( $path, '/' ) . '/';
+				},
+			)
+		);
+	}
 
-  public function test_happy() {
-    $plugin = Plugin::instance();
+	public function tearDown(): void {
+		WP_Mock::tearDown();
+	}
 
-    $this->assertSame($plugin->get_plugin_version(), '1.0.0');
-    $this->assertSame($plugin->is_plugin_initialized(), true);
-  }
+	public function test_happy() {
+		$plugin = Plugin::instance();
+
+		$this->assertSame( $plugin->get_plugin_version(), '1.0.0' );
+		$this->assertSame( $plugin->is_plugin_initialized(), true );
+	}
+
+	public function test_it_is_a_singleton() {
+		$a = Plugin::instance();
+		$b = Plugin::instance();
+
+		$this->assertSame( $a, $b );
+	}
+
+	public function test_actions() {
+		$this->assertSame( true, has_action( 'init', array( 'Blocks', 'register_blocks' ) ) );
+		$this->assertSame( true, has_action( 'init', array( 'PostTypes', 'register_post_type' ) ) );
+		$this->assertSame( true, has_action( 'add_meta_boxes', array( 'PostTypes', 'add_meta_boxes' ) ) );
+		$this->assertSame( true, has_action( 'save_post', array( 'PostTypes', 'save_metabox' ) ) );
+	}
 }
--- literati-example/tests/bootstrap.php
+++ literati-example/tests/bootstrap.php
@@ -1,8 +1,13 @@
 <?php
+/**
+ * Test bootstrap file.
+ *
+ * @package Literati\Example\Tests
+ */
 
-// First we need to load the composer autoloader, so we can use WP Mock
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+// First we need to load the composer autoloader, so we can use WP Mock.
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
-// Bootstrap WP_Mock to initialize built-in features
-WP_Mock::setUsePatchwork(true);
+// Bootstrap WP_Mock to initialize built-in features.
+WP_Mock::setUsePatchwork( true );
 WP_Mock::bootstrap();
